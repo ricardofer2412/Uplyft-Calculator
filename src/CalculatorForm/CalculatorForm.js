91d898070461f9b7c818610245441e24be5d409f
ricardofer2412 <ricardofer2412@hotmail.com>
2022-01-30 20:41:07 -0500

    add simple date

--- src/CalculatorForm/CalculatorForm.js
+++ src/CalculatorForm/CalculatorForm.js
@@ -3,6 +3,49 @@ import TextField from "@mui/material/TextField";
 import "./index.css";
 
 export default class CalculatorForm extends Component {
+  state = {
+    startDate: new Date(),
+    loanAmount: 0,
+    installmentInterval: "",
+    installmentAmount: 0,
+    interestRate: 0,
+  };
+
+  calculatePayment = (e) => {
+    e.preventDefault();
+
+    let paymentDatesList = [];
+
+    let loanAmount = 10000;
+
+    let startDate = new Date();
+
+    let installmentAmount = 1000;
+
+    let interestRate = 25 / 100;
+
+    let totalInterest = loanAmount * interestRate;
+
+    let totalAmount = totalInterest + loanAmount;
+
+    let paymentsNumber = totalAmount / installmentAmount;
+
+    let installmentInterval = "monthly";
+
+    if (installmentInterval === "monthly") {
+      console.log(totalAmount);
+      console.log(paymentsNumber);
+      startDate.setDate(startDate.getDate() + 60);
+      console.log(startDate);
+
+      for (let i = 0; i < paymentsNumber; i++) {
+        let newDateToArray = startDate.setDate(startDate.getDate() + 30);
+
+        paymentDatesList.push(newDateToArray);
+      }
+      console.log(paymentDatesList);
+    }
+  };
   render() {
     return (
       <div className="cal-form-div">
@@ -36,8 +79,8 @@ export default class CalculatorForm extends Component {
               label="Simple Interest Rate(%)"
               variant="standard"
             />
-            <button>Calculate</button>
-            <button>Clear</button>
+            <button onClick={this.calculatePayment}>Calculate</button>
+            <button onClick={this.calculatePayment}>Clear</button>
           </div>
         </form>
       </div>
